abdb7e64182475280cf564db36aae2315687f5ab
PeterYu789 <peter20146a17@gmail.com>
2024-04-23 14:56:23 +0800

    include a variable "countryRegion" that contain all the countries in each region

--- src/main/java/comp3111/qsproject/QSList.java
+++ src/main/java/comp3111/qsproject/QSList.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Set;
 
 /**
@@ -20,6 +21,7 @@ public class QSList {
     public static ObservableList<String> type = FXCollections.observableArrayList();
     public static ObservableList<String> region = FXCollections.observableArrayList();
     public static ObservableList<String> country = FXCollections.observableArrayList();
+    public static HashMap<String, ObservableList<String>> countryRegion = new HashMap<>();
 
     public static void initialize() {
         try {
@@ -30,6 +32,7 @@ public class QSList {
             Set<String> typeSet = new HashSet<>();
             Set<String> regionSet = new HashSet<>();
             Set<String> countrySet = new HashSet<>();
+            HashMap<String, HashSet<String>> countryRegionSet = new HashMap<>();
             while (reader.readRecord()) {
                 String[] row = reader.getValues();
                 QSItem qsItem = new QSItem(row);
@@ -37,12 +40,20 @@ public class QSList {
                 universitySet.add(qsItem.getName());
                 typeSet.add(qsItem.getType());
                 regionSet.add(row[7]);
+                if (!countryRegionSet.containsKey(row[7])) {
+                    countryRegionSet.put(row[7], new HashSet<>());
+                }
                 countrySet.add(qsItem.getCountry());
+                countryRegionSet.get(row[7]).add(qsItem.getCountry());
             }
             university.addAll(universitySet);
             type.addAll(typeSet);
             region.addAll(regionSet);
             country.addAll(countrySet);
+            for (String key : countryRegionSet.keySet()) {
+                countryRegion.put(key, FXCollections.observableArrayList());
+                countryRegion.get(key).addAll(countryRegionSet.get(key));
+            }
             reader.close();
         }
         catch (IOException e) {
